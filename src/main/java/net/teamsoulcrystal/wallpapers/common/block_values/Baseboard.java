e71ea360347bd7b921a295e858eb8650e41ba33d
Maker <foxy440000@gmail.com>
2023-10-28 12:05:21 +0300

    test

---
+++ src/main/java/net/teamsoulcrystal/wallpapers/common/block_values/Baseboard.java
@@ -0,0 +1,23 @@
+package net.teamsoulcrystal.wallpapers.common.block_values;
+
+import net.minecraft.util.StringRepresentable;
+
+public enum Baseboard implements StringRepresentable {
+    NONE("none"),
+    DRIPSTONE("dripstone"),
+    QUARTZ("quartz"),
+    GOLD("gold"),
+    PRISMARINE("prismarine");
+
+
+    private final String name;
+
+    Baseboard(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getSerializedName() {
+        return name;
+    }
+}
---
+++ src/main/java/net/teamsoulcrystal/wallpapers/common/tags/ModBlockTags.java
@@ -0,0 +1,17 @@
+package net.teamsoulcrystal.wallpapers.common.tags;
+
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import net.teamsoulcrystal.wallpapers.Wallpapers;
+
+public class ModBlockTags {
+    public static final TagKey<Block> WALLPAPER_BLOCKS = tag("wallpaper_blocks");
+
+    private static TagKey<Block> tag(String name) {
+        return BlockTags.create(new ResourceLocation(Wallpapers.MOD_ID, name));
+    }
+
+}
