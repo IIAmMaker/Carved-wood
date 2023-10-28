9c83f2ebbf5e6f8af9645e4e802e0f53af12ca70
Maker <foxy440000@gmail.com>
2023-10-28 14:04:25 +0300

    Change the "RedWallpaperBlock" to "WallpaperBlock"

--- src/main/java/net/teamsoulcrystal/wallpapers/common/block/wallpaper_blocks/WallpaperBlock.java
+++ src/main/java/net/teamsoulcrystal/wallpapers/common/block/wallpaper_blocks/WallpaperBlock.java
@@ -4,13 +4,13 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
-import net.teamsoulcrystal.wallpapers.common.block_values.Baseboard;
-import net.teamsoulcrystal.wallpapers.common.block_values.ModBlockStateProperties;
+import net.teamsoulcrystal.wallpapers.common.block.block_values.Baseboard;
+import net.teamsoulcrystal.wallpapers.common.block.block_values.ModBlockStateProperties;
 
-public class RedWallpaperBlock extends Block {
+public class WallpaperBlock extends Block {
     public static final EnumProperty<Baseboard> BASEBOARD = ModBlockStateProperties.BASEBOARD;
 
-    public RedWallpaperBlock(Properties pProperties) {
+    public WallpaperBlock(Properties pProperties) {
         super(pProperties);
         this.registerDefaultState(this.stateDefinition.any().setValue(BASEBOARD, Baseboard.NONE));
     }
