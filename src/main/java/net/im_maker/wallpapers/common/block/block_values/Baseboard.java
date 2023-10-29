8866353792f55766f5740239675bfdda730df9f0
Maker <foxy440000@gmail.com>
2023-10-29 16:15:40 +0300

    Revert "Revert ".""
    This reverts commit 09bee9614c2e0f269f15771bb7e72ed25a124053.

---
+++ src/main/java/net/im_maker/wallpapers/common/block/block_values/Baseboard.java
@@ -0,0 +1,23 @@
+package net.im_maker.wallpapers.common.block.block_values;
+
+import net.minecraft.util.StringRepresentable;
+
+public enum Baseboard implements StringRepresentable {
+    NONE("none"),
+    DRIPSTONE("dripstone"),
+    QUARTZ("quartz"),
+    GOLD("gold"),
+    PRISMARINE("prismarine");
+
+
+    private final String name;
+
+    Baseboard(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getSerializedName() {
+        return name;
+    }
+}
