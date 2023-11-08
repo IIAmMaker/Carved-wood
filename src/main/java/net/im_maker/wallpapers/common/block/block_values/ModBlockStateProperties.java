5ab13ac1e3a85f51e1406eaa83cfc0349c4a2e64
Maker <foxy440000@gmail.com>
2023-11-08 17:42:21 +0300

    Paint

--- src/main/java/net/im_maker/wallpapers/common/block/block_values/ModBlockStateProperties.java
+++
@@ -1,8 +0,0 @@
-package net.im_maker.wallpapers.common.block.block_values;
-
-import net.minecraft.world.level.block.state.properties.*;
-
-public class ModBlockStateProperties {
-    public static final EnumProperty<Baseboard> BASEBOARD = EnumProperty.create("baseboard", Baseboard.class);
-}
-
