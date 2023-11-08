f7c617a19b71ad98d58f9e89be28fef9912398e2
Maker <foxy440000@gmail.com>
2023-11-08 20:38:56 +0300

    Paint

--- src/main/java/net/im_maker/paint/common/block/ModBlocks.java
+++ src/main/java/net/im_maker/paint/common/block/ModBlocks.java
@@ -4,6 +4,9 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -18,6 +21,14 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Paint.MOD_ID);
     //Wallpaper Blocks
+    public static final RegistryObject<Block> PAINTED_PLANKS = registryBlock("painted_planks",
+            () -> new Block (BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.COLOR_RED).strength(2.0F, 3.0F).sound(SoundType.WOOD)), CreativeModeTab.TAB_BUILDING_BLOCKS);
+    public static final RegistryObject<Block> PAINTED_BRICKS = registryBlock("painted_bricks",
+            () -> new Block (BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.COLOR_RED).strength(2.0F, 3.0F).sound(SoundType.STONE)), CreativeModeTab.TAB_BUILDING_BLOCKS);
+    public static final RegistryObject<Block> PAINT_BUCKET = registryBlock("paint_bucket",
+            () -> new PaintBucketBlock (BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.COLOR_RED).strength(2.0F, 3.0F).sound(SoundType.LANTERN)), CreativeModeTab.TAB_BUILDING_BLOCKS);
+    public static final RegistryObject<Block> MAKER_PAINT_BUCKET = registryBlock("maker_paint_bucket",
+            () -> new Block (BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.COLOR_RED).strength(2.0F, 3.0F).sound(SoundType.LANTERN)), CreativeModeTab.TAB_BUILDING_BLOCKS);
 
     private static <T extends Block> RegistryObject<T> registryBlock(String name, Supplier<T> block, CreativeModeTab tab ) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
---
+++ src/main/java/net/im_maker/paint/common/block/PaintBucketBlock.java
@@ -0,0 +1,20 @@
+package net.im_maker.paint.common.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class PaintBucketBlock extends Block {
+    public PaintBucketBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+        return box(4, 0, 4, 12, 10, 12);
+    }
+
+}
