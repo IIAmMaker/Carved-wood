49ce597ea4604bfc103137d3254b6a9af89917af
Maker <foxy440000@gmail.com>
2023-12-14 17:21:24 +0300

    Paintable 0.8

--- src/main/java/net/im_maker/paintable/common/item/paint_brushs/PaintBrushItem.java
+++ src/main/java/net/im_maker/paintable/common/item/paint_brushs/PaintBrushItem.java
@@ -6,6 +6,7 @@ import net.im_maker.paintable.common.item.ModItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -22,108 +23,45 @@ public class PaintBrushItem extends Item {
     @Override
     public InteractionResult useOn(UseOnContext context) {
         Level world = context.getLevel();
-        BlockPos blockpos = context.getClickedPos();
+        BlockPos blockPos = context.getClickedPos();
         Player player = context.getPlayer();
-        ItemStack itemstack = context.getItemInHand();
-        BlockState blockstate = world.getBlockState(blockpos);
-        if (blockstate.is(ModBlocks.RED_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.RED_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.ORANGE_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.ORANGE_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.YELLOW_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.YELLOW_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.LIME_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.LIME_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.GREEN_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.GREEN_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.CYAN_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.CYAN_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.LIGHT_BLUE_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.LIGHT_BLUE_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.BLUE_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.BLUE_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.PURPLE_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.PURPLE_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.MAGENTA_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.MAGENTA_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.PINK_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.PINK_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.BROWN_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.BROWN_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.BLACK_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.BLACK_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.GRAY_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.GRAY_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.LIGHT_GRAY_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.LIGHT_GRAY_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else if (blockstate.is(ModBlocks.WHITE_PAINT_BUCKET.get()) && blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT) != 0) {
-            context.getPlayer().setItemInHand(context.getHand(), new ItemStack(ModItems.WHITE_PAINT_BRUSH.get()));
-            world.playSound((Player)null, blockpos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-            Integer a = blockstate.getValue(ModBlockStateProperties.LEVEL_PAINT);
-            world.setBlockAndUpdate(blockpos, blockstate.setValue(ModBlockStateProperties.LEVEL_PAINT, a-1));
-            return InteractionResult.sidedSuccess(world.isClientSide);
-        } else {
-            return InteractionResult.PASS;
+        ItemStack itemStack = context.getItemInHand();
+        InteractionHand hand = context.getPlayer().getUsedItemHand();
+        BlockState blockState = world.getBlockState(blockPos);
+
+        if (isMatchingPaintBucketBlock(blockState)) {
+            int paintLevel = getPaintLevel(blockState);
+
+            if (paintLevel > 0) {
+                ItemStack newPaintBrush = createNewPaintBrush();
+                player.setItemInHand(hand, newPaintBrush); //itemStack -> Provided: itemtack
+                playSoundEffect(world);
+                updatePaintLevel(world, blockPos, paintLevel - 1);
+                return InteractionResult.sidedSuccess(world.isClientSide());
+            }
         }
+
+        return InteractionResult.PASS;
+    }
+
+    private boolean isMatchingPaintBucketBlock(BlockState blockState) {
+        return blockState.is(ModBlocks.PAINT_BUCKET.get()) && blockState.getValue(ModBlockStateProperties.LEVEL_PAINT) > 0;
+    }
+
+    private int getPaintLevel(BlockState blockState) {
+        return blockState.getValue(ModBlockStateProperties.LEVEL_PAINT);
+    }
+
+    private ItemStack createNewPaintBrush() {
+        return new ItemStack(ModItems.PAINT_BRUSH.get());
+    }
+
+    private void playSoundEffect(Level world) {
+        // Code to play the sound effect
+    }
+
+    private void updatePaintLevel(Level world, BlockPos blockPos, int newPaintLevel) {
+        BlockState updatedBlockState = world.getBlockState(blockPos).setValue(ModBlockStateProperties.LEVEL_PAINT, newPaintLevel);
+        world.setBlockAndUpdate(blockPos, updatedBlockState);
     }
 }
