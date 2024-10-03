e59d871a0d4c6fff0188bafd488fcc2dc9af1050
IIAmMaker <foxy440000@gmail.com>
2024-10-03 20:23:11 +0300

    Carved Wood

---
+++ src/main/java/net/im_maker/carved_wood/common/block/block_values/CWBlockStateProperties.java
@@ -0,0 +1,7 @@
+package net.im_maker.carved_wood.common.block.block_values;
+
+import net.minecraft.world.level.block.state.properties.*;
+
+public class CWBlockStateProperties {
+    public static final IntegerProperty LEVEL_PAINT = IntegerProperty.create("paint_level", 0, 4);
+}
