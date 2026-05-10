9ff874ea8dc11025812d196fe6aaf7dc0c5c97ec
IIAmMaker <foxy440000@gmail.com>
2026-05-10 14:50:55 +0300

    Add FlammableBlockRegistry

--- fabric/src/main/java/net/im_maker/carved_wood/common/block/custom/PlanksBlock.java
+++ fabric/src/main/java/net/im_maker/carved_wood/common/block/custom/PlanksBlock.java
@@ -4,10 +4,8 @@ import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.minecraft.world.level.block.Block;
 
 public class PlanksBlock extends Block {
-    private final Boolean isFlammable;
     public PlanksBlock(Properties pProperties, Boolean isFlammable) {
         super(pProperties);
-        this.isFlammable = isFlammable;
         if (isFlammable) FlammableBlockRegistry.getDefaultInstance().add(this, 20, 5);
     }
     public PlanksBlock(Properties pProperties) {
